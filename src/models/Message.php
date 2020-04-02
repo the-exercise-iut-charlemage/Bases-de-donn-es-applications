8d04e6fc86d3ee4fceb7d5c537845d3556b98522
Ronard88 <62329422+Ronard88@users.noreply.github.com>
2020-04-02 18:39:44 +0200

    ajout message.php et user.php

---
+++ src/models/Message.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace appdb\models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    protected $table = 'message';
+    protected $primaryKey = 'id';
+    protected $fillable = ['id', 'title', 'content'];
+    public $timestamps = true;
+
+    
+}
---
+++ src/models/User.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace appdb\models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table = 'user';
+    protected $primaryKey = 'id';
+    protected $fillable = ['id', 'email', 'name', 'surname', 'adress', 'phone', 'birth'];
+    public $timestamps = false;
+
+    
+}
